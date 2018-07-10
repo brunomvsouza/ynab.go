0fe3b8954c9c39a39655aab863f3635ef81b000a
Bruno M V Souza <signed@bmvs.dev>
2018-07-11 00:33:36 +0200

    Add account representation

---
+++ account/account.go
@@ -0,0 +1,15 @@
+package account
+
+// Account represents an account
+type Account struct {
+	ID               string  `json:"id"`
+	Name             string  `json:"name"`
+	Type             Type    `json:"type"`
+	OnBudget         bool    `json:"on_budget"`
+	Note             *string `json:"note"`
+	Balance          int64   `json:"balance"`
+	ClearedBalance   int64   `json:"cleared_balance"`
+	UnclearedBalance int64   `json:"uncleared_balance"`
+	Closed           bool    `json:"closed"`
+	Deleted          bool    `json:"deleted"`
+}
