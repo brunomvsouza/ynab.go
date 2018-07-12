365cfb01b44ec102ad8ab7b267299ba9a8824608
Bruno M V Souza <hi@brunosouza.org>
2018-07-13 00:33:39 +0200

    Add transaction service's GetScheduledTransactions support

--- api/transaction/service_test.go
+++ api/transaction/service_test.go
@@ -76,33 +76,35 @@ func TestService_GetTransactions(t *testing.T) {
 	expectedSubTransactionPayeeID := "6216ab4b-bb05-4574-b4b5-be2dee26ab0d"
 	expectedSubTransactionCategoryID := "080985e4-4175-43e4-96bb-d207a9d2c8ce"
 
-	expected := &transaction.Transaction{
-		ID:           "e6ad88f5-6f16-4480-9515-5377012750dd",
-		Date:         expectedDate,
-		Amount:       int64(-43950),
-		Memo:         &expectedMemo,
-		Cleared:      transaction.ClearingStatusReconciled,
-		Approved:     true,
-		AccountID:    "09eaca5e-6f16-4480-9515-828fb90638f2",
-		AccountName:  "Bank Name",
-		PayeeID:      &expectedPayeeID,
-		PayeeName:    &expectedPayeeName,
-		CategoryID:   &expectedCategoryID,
-		CategoryName: &expectedCategoryName,
-		Deleted:      false,
-		SubTransactions: []*transaction.SubTransaction{
-			{
-				ID:            "9453526b-2f58-4c02-9683-a30c2a1192d7",
-				TransactionID: "e6ad88f5-6f16-4480-9515-5377012750dd",
-				Amount:        int64(-33970),
-				Memo:          &expectedSubTransactionMemo,
-				PayeeID:       &expectedSubTransactionPayeeID,
-				CategoryID:    &expectedSubTransactionCategoryID,
-				Deleted:       false,
+	expected := []*transaction.Transaction{
+		{
+			ID:           "e6ad88f5-6f16-4480-9515-5377012750dd",
+			Date:         expectedDate,
+			Amount:       int64(-43950),
+			Memo:         &expectedMemo,
+			Cleared:      transaction.ClearingStatusReconciled,
+			Approved:     true,
+			AccountID:    "09eaca5e-6f16-4480-9515-828fb90638f2",
+			AccountName:  "Bank Name",
+			PayeeID:      &expectedPayeeID,
+			PayeeName:    &expectedPayeeName,
+			CategoryID:   &expectedCategoryID,
+			CategoryName: &expectedCategoryName,
+			Deleted:      false,
+			SubTransactions: []*transaction.SubTransaction{
+				{
+					ID:            "9453526b-2f58-4c02-9683-a30c2a1192d7",
+					TransactionID: "e6ad88f5-6f16-4480-9515-5377012750dd",
+					Amount:        int64(-33970),
+					Memo:          &expectedSubTransactionMemo,
+					PayeeID:       &expectedSubTransactionPayeeID,
+					CategoryID:    &expectedSubTransactionCategoryID,
+					Deleted:       false,
+				},
 			},
 		},
 	}
-	assert.Equal(t, expected, transactions[0])
+	assert.Equal(t, expected, transactions)
 }
 
 func TestService_GetTransaction(t *testing.T) {
@@ -352,23 +354,25 @@ func TestService_GetTransactionsByCategory(t *testing.T) {
 	expectedCategoryID := "a33c906e-444c-469c-be27-04c8e0c9959f"
 	expectedCategoryName := "Rent"
 
-	expected := &transaction.Hybrid{
-		Type:         transaction.TypeTransaction,
-		ID:           "c132c55c-1200-4606-a321-99f4ec24b4df",
-		Date:         expectedDate,
-		Amount:       int64(-42000),
-		Memo:         &expectedMemo,
-		Cleared:      transaction.ClearingStatusReconciled,
-		Approved:     true,
-		AccountID:    "134d159-444c-469c-be27-44094e388fa0",
-		AccountName:  "Cash",
-		PayeeID:      &expectedPayeeID,
-		PayeeName:    &expectedPayeeName,
-		CategoryID:   &expectedCategoryID,
-		CategoryName: &expectedCategoryName,
-		Deleted:      false,
+	expected := []*transaction.Hybrid{
+		{
+			Type:         transaction.TypeTransaction,
+			ID:           "c132c55c-1200-4606-a321-99f4ec24b4df",
+			Date:         expectedDate,
+			Amount:       int64(-42000),
+			Memo:         &expectedMemo,
+			Cleared:      transaction.ClearingStatusReconciled,
+			Approved:     true,
+			AccountID:    "134d159-444c-469c-be27-44094e388fa0",
+			AccountName:  "Cash",
+			PayeeID:      &expectedPayeeID,
+			PayeeName:    &expectedPayeeName,
+			CategoryID:   &expectedCategoryID,
+			CategoryName: &expectedCategoryName,
+			Deleted:      false,
+		},
 	}
-	assert.Equal(t, expected, transactions[0])
+	assert.Equal(t, expected, transactions)
 }
 
 func TestService_GetTransactionsByPayee(t *testing.T) {
@@ -425,23 +429,97 @@ func TestService_GetTransactionsByPayee(t *testing.T) {
 	expectedCategoryID := "a33c906e-444c-469c-be27-04c8e0c9959f"
 	expectedCategoryName := "Rent"
 
-	expected := &transaction.Hybrid{
-		Type:         transaction.TypeTransaction,
-		ID:           "c132c55c-1200-4606-a321-99f4ec24b4df",
-		Date:         expectedDate,
-		Amount:       int64(-42000),
-		Memo:         &expectedMemo,
-		Cleared:      transaction.ClearingStatusReconciled,
-		Approved:     true,
-		AccountID:    "134d159-444c-469c-be27-44094e388fa0",
-		AccountName:  "Cash",
-		PayeeID:      &expectedPayeeID,
-		PayeeName:    &expectedPayeeName,
-		CategoryID:   &expectedCategoryID,
-		CategoryName: &expectedCategoryName,
-		Deleted:      false,
+	expected := []*transaction.Hybrid{
+		{
+			Type:         transaction.TypeTransaction,
+			ID:           "c132c55c-1200-4606-a321-99f4ec24b4df",
+			Date:         expectedDate,
+			Amount:       int64(-42000),
+			Memo:         &expectedMemo,
+			Cleared:      transaction.ClearingStatusReconciled,
+			Approved:     true,
+			AccountID:    "134d159-444c-469c-be27-44094e388fa0",
+			AccountName:  "Cash",
+			PayeeID:      &expectedPayeeID,
+			PayeeName:    &expectedPayeeName,
+			CategoryID:   &expectedCategoryID,
+			CategoryName: &expectedCategoryName,
+			Deleted:      false,
+		},
 	}
-	assert.Equal(t, expected, transactions[0])
+	assert.Equal(t, expected, transactions)
+}
+
+func TestService_GetScheduledTransactions(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	url := "https://api.youneedabudget.com/v1/budgets/aa248caa-eed7-4575-a990-717386438d2c/scheduled_transactions"
+	httpmock.RegisterResponder("GET", url,
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(200, `{
+  "data": {
+    "scheduled_transactions": [
+      {
+        "id": "56f4fc86-2ed7-4b3b-9116-7a214261b3cd",
+        "date_first": "2018-11-13",
+        "date_next": "2018-11-13",
+        "frequency": "never",
+        "amount": -9000,
+        "memo": "nice memo",
+        "flag_color": "yellow",
+        "account_id": "09eaca5e-312a-4bcd-89c4-828fb90638f2",
+        "account_name": "Bank Name",
+        "payee_id": "0d0e928d-312a-4bcd-89c4-e02f40d1fe46",
+        "payee_name": "bla bla bla",
+        "category_id": "f3cc4f55-312a-4bcd-89c4-db34379cb1dc",
+        "category_name": "Yearly subscription",
+        "transfer_account_id": null,
+        "deleted": false,
+        "subtransactions": []
+      }
+		]
+	}
+}
+		`), nil
+		},
+	)
+
+	client := ynab.NewClient("")
+	transactions, err := client.Transaction().GetScheduledTransactions(
+		"aa248caa-eed7-4575-a990-717386438d2c")
+	assert.NoError(t, err)
+
+	expectedFirstAndLastDate, err := api.NewDateFromString("2018-11-13")
+	assert.NoError(t, err)
+
+	expectedMemo := "nice memo"
+	expectedFlagColor := transaction.FlagColorYellow
+	expectedPayeeID := "0d0e928d-312a-4bcd-89c4-e02f40d1fe46"
+	expectedPayeeName := "bla bla bla"
+	expectedCategoryID := "f3cc4f55-312a-4bcd-89c4-db34379cb1dc"
+	expectedCategoryName := "Yearly subscription"
+
+	expected := []*transaction.Scheduled{
+		{
+			ID:              "56f4fc86-2ed7-4b3b-9116-7a214261b3cd",
+			DateFirst:       expectedFirstAndLastDate,
+			DateNext:        expectedFirstAndLastDate,
+			Frequency:       transaction.FrequencyNever,
+			Amount:          int64(-9000),
+			Memo:            &expectedMemo,
+			FlagColor:       &expectedFlagColor,
+			AccountID:       "09eaca5e-312a-4bcd-89c4-828fb90638f2",
+			AccountName:     "Bank Name",
+			PayeeID:         &expectedPayeeID,
+			PayeeName:       &expectedPayeeName,
+			CategoryID:      &expectedCategoryID,
+			CategoryName:    &expectedCategoryName,
+			Deleted:         false,
+			SubTransactions: []*transaction.ScheduledSubTransaction{},
+		},
+	}
+	assert.Equal(t, expected, transactions)
 }
 
 func TestFilter_ToQuery(t *testing.T) {
