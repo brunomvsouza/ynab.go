afded830beef0bc848b35b2b89966c307f8b1bb0
Bruno M V Souza <signed@bmvs.dev>
2018-07-12 17:05:25 +0200

    Add month service support

---
+++ api/month/service.go
@@ -0,0 +1,50 @@
+package month
+
+import (
+	"fmt"
+
+	"bmvs.io/ynab/api"
+)
+
+// NewService facilitates the creation of a new month service instance
+func NewService(c api.Getter) *Service {
+	return &Service{c}
+}
+
+// Service wraps YNAB payee API endpoints
+type Service struct {
+	c api.Getter
+}
+
+// GetMonths fetches the list of months from a budget
+// https://api.youneedabudget.com/v1#/Months/getBudgetMonths
+func (s *Service) GetMonths(budgetID string) ([]*Month, error) {
+	resModel := struct {
+		Data struct {
+			Months []*Month `json:"months"`
+		} `json:"data"`
+	}{}
+
+	url := fmt.Sprintf("/budgets/%s/months", budgetID)
+	if err := s.c.GET(url, &resModel); err != nil {
+		return nil, err
+	}
+	return resModel.Data.Months, nil
+}
+
+// GetMonth fetches a specific month from a budget
+// https://api.youneedabudget.com/v1#/Months/getBudgetMonth
+func (s *Service) GetMonth(budgetID string, month api.Date) (*Month, error) {
+	resModel := struct {
+		Data struct {
+			Month *Month `json:"month"`
+		} `json:"data"`
+	}{}
+
+	url := fmt.Sprintf("/budgets/%s/months/%s", budgetID,
+		month.Format(api.DateLayout))
+	if err := s.c.GET(url, &resModel); err != nil {
+		return nil, err
+	}
+	return resModel.Data.Month, nil
+}
