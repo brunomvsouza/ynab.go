024485a18543c6276c49613a3e0eb5be171e7808
Bruno M V Souza <hi@brunosouza.org>
2018-07-12 02:41:44 +0200

    Add category service's GetCategories support

--- api/category/entity.go
+++ api/category/entity.go
@@ -2,23 +2,30 @@ package category
 
 // Category represents a category
 type Category struct {
-	ID                      string `json:"id"`
-	CategoryGroupID         string `json:"category_group_id"`
-	OriginalCategoryGroupID string `json:"original_category_group_id"`
-	Name                    string `json:"name"`
-	Hidden                  bool   `json:"hidden"`
-	Budgeted                int64  `json:"budgeted"`
-	Activity                int64  `json:"activity"`
-	Balance                 int64  `json:"balance"`
-	Deleted                 bool   `json:"deleted"`
+	ID              string `json:"id"`
+	CategoryGroupID string `json:"category_group_id"`
+	Name            string `json:"name"`
+	Hidden          bool   `json:"hidden"`
+	Budgeted        int64  `json:"budgeted"`
+	Activity        int64  `json:"activity"`
+	Balance         int64  `json:"balance"`
+	Deleted         bool   `json:"deleted"`
 
-	Note *string `json:"note"`
+	Note                    *string `json:"note"`
+	OriginalCategoryGroupID *string `json:"original_category_group_id"`
 }
 
-// Group represents a category group
-type Group struct {
+// ResumedGroup represents a resumed category group
+type ResumedGroup struct {
 	ID      string `json:"id"`
 	Name    string `json:"name"`
 	Hidden  bool   `json:"hidden"`
 	Deleted bool   `json:"deleted"`
+}
+
+// Group represents a category group
+type Group struct {
+	ResumedGroup
+
+	Categories []*Category `json:"categories"`
 }
