2f11bb4cd9865421516535e703545ad0fee9d05e
Bruno M V Souza <hi@brunosouza.org>
2018-08-01 00:14:21 +0200

    Add support to the new fields for month requests

--- api/month/entity.go
+++ api/month/entity.go
@@ -16,6 +16,15 @@ type Month struct {
 	Note         *string `json:"note"`
 	ToBeBudgeted *int64  `json:"to_be_budgeted"`
 	AgeOfMoney   *int64  `json:"age_of_money"`
+
+	// Income the total amount in transactions categorized to "Inflow: To be Budgeted"
+	// in the month (milliunits format)
+	Income *int64 `json:"income"`
+	// Budgeted the total amount budgeted in the month (milliunits format)
+	Budgeted *int64 `json:"budgeted"`
+	// Activity the total amount in transactions in the month, excluding those
+	// categorized to "Inflow: To be Budgeted" (milliunits format)
+	Activity *int64 `json:"activity"`
 }
 
 // Summary represents the summary of a month for a budget
@@ -28,4 +37,13 @@ type Summary struct {
 	Note         *string `json:"note"`
 	ToBeBudgeted *int64  `json:"to_be_budgeted"`
 	AgeOfMoney   *int64  `json:"age_of_money"`
+
+	// Income the total amount in transactions categorized to "Inflow: To be Budgeted"
+	// in the month (milliunits format)
+	Income *int64 `json:"income"`
+	// Budgeted the total amount budgeted in the month (milliunits format)
+	Budgeted *int64 `json:"budgeted"`
+	// Activity the total amount in transactions in the month, excluding those
+	// categorized to "Inflow: To be Budgeted" (milliunits format)
+	Activity *int64 `json:"activity"`
 }
