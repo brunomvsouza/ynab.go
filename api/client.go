606741949a8245ac7ca8ef0f8e7c0031492bf589
Bruno M V Souza <signed@bmvs.dev>
2018-08-11 12:33:10 +0200

    Add licence on files and package descriptions

--- api/client.go
+++ api/client.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2018, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
+// Package api implements shared structures and behaviours of
+// the API services
 package api // import "go.bmvs.io/ynab/api"
 
 // ClientReader contract for a read only client
--- api/user/entity.go
+++ api/user/entity.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2018, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
+// Package user implements transaction user and services
 package user // import "go.bmvs.io/ynab/api/user"
 
 // User represents an user
