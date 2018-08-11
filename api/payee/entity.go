5cdaf65a2310f23449c853ac3b9ce71bc0651ad9
Bruno M V Souza <hi@brunosouza.org>
2018-08-11 12:33:10 +0200

    Add licence on files and package descriptions

--- api/payee/entity.go
+++ api/payee/entity.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2018, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
+// Package payee implements payee entities and services
 package payee // import "go.bmvs.io/ynab/api/payee"
 
 // Payee represents a payee for a budget
--- api/payee/service.go
+++ api/payee/service.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
 package payee
 
 import (
