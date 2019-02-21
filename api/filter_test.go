c6159cd2433a250e74b65e680f5932bc9ce03d87
Bruno M V Souza <signed@bmvs.dev>
2019-02-21 21:09:49 +0100

    Add support for delta requests while retrieving accounts

---
+++ api/filter_test.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2019, Bruno M V Souza <github@b.bmvs.io>. All rights reserved.
+// Use of this source code is governed by a BSD-2-Clause license that can be
+// found in the LICENSE file.
+
+package api_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.bmvs.io/ynab/api"
+)
+
+func TestFilter_ToQuery(t *testing.T) {
+	table := []struct {
+		Input  api.Filter
+		Output string
+	}{
+		{
+			Input:  api.Filter{LastKnowledgeOfServer: 2},
+			Output: "last_knowledge_of_server=2",
+		},
+		{
+			Input:  api.Filter{LastKnowledgeOfServer: 0},
+			Output: "last_knowledge_of_server=0",
+		},
+		{
+			Input:  api.Filter{},
+			Output: "last_knowledge_of_server=0",
+		},
+	}
+
+	for _, test := range table {
+		assert.Equal(t, test.Output, test.Input.ToQuery())
+	}
+}
