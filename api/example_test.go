c198975caeadf4edf6c4cd31694ea5d0deff4767
Mike Schilli <github@perlmeister.com>
2025-02-05 11:17:14 -0800

    Ynab API servers discontinued sending the X-Rate-Limit header in late 2023 (https://github.com/ynab/ynab-sdk-js/issues/164) which now breaks the client ("invalid rate limit string"). There's no way to obtain the current rate limit via the API (https://api.ynab.com/#rate-limiting), so it's probably best to rip out all related functions. This will break anyone relying on the public RateLimit() function, but their applications are already broken throwing runtime errors because of the missing header.

--- api/example_test.go
+++ api/example_test.go
@@ -16,10 +16,3 @@ func ExampleDateFromString() {
 
 	// Output: 2020-01-20 00:00:00 +0000 UTC
 }
-
-func ExampleParseRateLimit() {
-	r, _ := api.ParseRateLimit("1/200")
-	fmt.Println(r.Used())
-
-	// Output: 1
-}
